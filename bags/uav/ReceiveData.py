1c8a174f9160a90f2d2e3c249d15384e2f8df614
TitouanV <titouan.verdu@imt-lille-douai.fr>
2021-07-02 12:00:59 +0200

    Changement code pour GPIO, correctif erreur

--- bags/uav/ReceiveData.py
+++ bags/uav/ReceiveData.py
@@ -1,4 +1,4 @@
-l# Copyright 2017, Digi International Inc.
+# Copyright 2017, Digi International Inc.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
